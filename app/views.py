0e54a80c54fc1ca79941e300719977fb67b0015f
BlaineLambert <flambert24@basecampcodingacademy.org>
2023-11-06 13:41:43 -0600

    done

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.http.request import HttpRequest
 
 # Create your views here.
 def hello_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello World")
+    return HttpResponse("Hello World!")
